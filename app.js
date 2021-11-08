7916e5363816c3cba9327305b14471932cb45076
sara_h <smoke_screen16@hotmail.com>
2021-11-07 23:47:29 -0500

    Created an array for the card options

---
+++ app.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', () => {
+    //card options
+    const cardArray = [
+        {
+            name: 'fries',
+            img: 'images/fries.png'
+        },
+        {
+            name: 'cheeseburger',
+            img: 'images/chesseburger.png'
+        }, 
+        {
+            name: 'blank',
+            img: 'images/blank.png'
+        },
+        {
+            name: 'hotdog',
+            img: 'images/hotdog.png'
+        },
+        {
+            name: 'ice cream',
+            img: 'images/ice-cream.png'
+        },
+        {
+            name: 'milkshake',
+            img: 'images/milkshake.png'
+        },
+        {
+            name: 'pizza',
+            img: 'images/pizza.png'
+        }, 
+        {
+            name: 'white',
+            img: 'images/white.png'
+        }
+    ]
+})
