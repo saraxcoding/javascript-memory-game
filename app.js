a002243fd6dda1a4882c9e4af159243ad3924a4b
sara_h <smoke_screen16@hotmail.com>
2021-11-08 23:18:21 -0500

    Set function to check for matches between flipped cards

--- app.js
+++ app.js
@@ -68,8 +68,10 @@ document.addEventListener('DOMContentLoaded', () => {
     ]
 
     const grid = document.querySelector('.grid')
+    const resultDisplay = document.querySelector('#result')
     var cardsChosen = [];
     var cardsChosenId = [];
+    var cardsWon = [];
 
     //create your board
     function createBoard() {
@@ -83,6 +85,24 @@ document.addEventListener('DOMContentLoaded', () => {
     } 
  
     //check for matches
+    function checkForMatch() {
+        var cards = document.querySelectorAll('img')
+        const optionOneId = cardsChosenId[0]
+        const optionTwoId = cardsChosenId[1]
+        if (cardsChosen[0] === cardsChosen[1]) {
+            alert('You found a match! HOORAY!');
+            cards[optionOneId].setAttribute('src', 'images/white.png');
+            cards[optionTwoId].setAttribute('src', 'images/white.png');
+            cardsWon.push(cardsChosen)
+        } else {
+            cards[optionOneId].setAttribute('src', 'image/white.png');
+            cards[optionTwoId].setAttribute('src', 'image/white.png');
+            alert('Sorry, try again!');
+        }
+        cardsChosen = [];
+        cardsChosenId = [];
+        resultDisplay.textContent = cardsWon.length;
+    }
 
     //flip your card
     function flipcard() {
